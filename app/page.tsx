eddd896d2f58f539cb9f34fbc4c3d7e673461ae7
Ming Chi <ming@state.com>
2025-12-23 16:07:57 +0000

    Hero section adjucted

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Navbar from "@/app/components/Navbar";
-import Hero from "@/app/components/Hero";
 import FeatureCard from "@/app/components/FeatureCard";
 import Footer from "@/app/components/Footer";
+import Hero from "@/app/components/Hero";
+import Navbar from "@/app/components/Navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <Navbar />
-      
-      <main className="bg-gray-100 flex-grow">
+
+      <main className="bg-gray-100 flex-grow pt-20">
         <Hero />
-        
+
         {/* Feature Cards Section */}
         <section className="py-12">
           <div className="max-w-6xl mx-auto px-8">
@@ -31,7 +31,7 @@ export default function Home() {
           </div>
         </section>
       </main>
-      
+
       <Footer />
     </div>
   );
